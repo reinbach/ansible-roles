4e7791377a5004be1c99b8cd1575268b8e3dbb93
Greg Reinbach <greg@reinbach.com>
2021-01-31 06:47:33 -0500

    Bump version to 0.12.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="bayete-roles",
-    version="0.11.0",
+    version="0.12.0",
     description=readme(),
     url="https://git.ironlabs.com/Bayete/roles",
     author="Greg Reinbach",
